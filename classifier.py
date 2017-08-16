31e0799b925d10bae0d72bc1f3a00e4f3df0b923
Jake Son <jake.son@childmind.org>
2017-08-16 16:09:27 -0400

    Added comments, Anirudh is taking over code.

--- classifier.py
+++ classifier.py
@@ -9,6 +9,7 @@ from itertools import groupby
 
 def learn():
 
+    # Manually created simulated data for personal learning -- should be automated for training/testing
     df = pd.DataFrame(data=np.array([[1,1,0,1,1,0,1],[0,0,1,0,0,1,0],[1,1,1,1,1,0,1],[1,0,0,1,1,0,0],
                                      [0,0,1,0,1,1,0],[0,1,0,1,1,0,1],[0,0,0,1,1,0,1],[1,0,1,0,0,1,0],
                                      [1,1,0,0,1,0,1],[0,0,1,0,0,0,0]]),
@@ -17,37 +18,39 @@ def learn():
     df['Dx'] = ['ASD', 'SM', 'ASD', 'ASD', 'SM', 'ASD', 'ASD', 'SM', 'ASD', 'SM']
     df['subset'] = ['train', 'train', 'train', 'train', 'train', 'train', 'train', 'train', 'test', 'test']
 
-    # Create array with Dx converted to a digit
-    # Access factorized array only with factorized[0]
-    # Access codes (strings for factorized array) with factorized[1]
+    # Create array with Dx converted to a digit so that Dx is codified
     factorized = pd.factorize(df['Dx'])
-    # print(df)
 
+    # Extract relevant features (questions in this case) from pd df
     features = df.columns[:7]
-    # print(features)
 
+    # Initialize random forest classifier with various parameters
     rnd_clf = RandomForestClassifier(n_estimators=10, random_state=0)
-    log_clf = LogisticRegression()
-    svm_clf = SVC()
 
-    voting_clf = VotingClassifier(estimators=[('lr', log_clf),('rf',rnd_clf),('svc',svm_clf)], voting='hard')
+    # Initialize logitistic regression
+    log_clf = LogisticRegression()
+
+    voting_clf = VotingClassifier(estimators=[('lr', log_clf),('rf',rnd_clf)], voting='hard')
 
     training_set = df[df['subset'] == 'train']
+    test_set = df[df['subset'] == 'test']
 
     voting_clf.fit(training_set[features], factorized[0][:-2])
     rnd_clf.fit(training_set[features], factorized[0][:-2])
 
-    test_set = df[df['subset'] == 'test']
-
     print(voting_clf.predict(test_set[features]))
     print(rnd_clf.predict(test_set[features]))
 
-    for clf in (log_clf, rnd_clf, svm_clf, voting_clf):
+    # Each model has a accuracy of 1.0 -- caused by data overfitting, will have to change simulated dataset
+    for clf in (log_clf, rnd_clf, voting_clf):
         clf.fit(training_set[features], factorized[0][:-2])
         y_pred = clf.predict(test_set[features])
         print(clf.__class__.__name__, accuracy_score(y_pred, factorized[0][-2:]))
 
+    # Print prediction probabilities for each Dx categorization for each "test" item
     print(rnd_clf.predict_proba(test_set[features]))
+
+    # Print feature importance
     print(list(zip(training_set[features], rnd_clf.feature_importances_)))
 
 def DSM():
@@ -59,10 +62,8 @@ def DSM():
     df = output.parse(sheetname)
     df = pd.DataFrame(data=df)
     df = df.set_index('EID')
-
     df = df.drop(['START_DATE.y', 'Study.y', 'Site.y', 'Year.y', 'Days_Baseline.y',
                   'Season.y'], axis=1)
-
     df = df.replace('NA', np.NaN)
 
     # Load Patient Dx
@@ -73,24 +74,13 @@ def DSM():
     dx = pd.DataFrame(data=dx)
     dx = dx.set_index('EID')
 
+    # Retain columns that are relevant for analysis, reduce pd df size
     col_keep = []
-
     for num in range(1, 8):
         col_keep.append('DX_0' + str(num) + '_Cat')
         col_keep.append('DX_0' + str(num) + '_Sub')
         col_keep.append('DX_0' + str(num) + '_Code')
-
     dx = dx[col_keep]
-
-    # Remove EIDs that have no Dx (would just add noise, "No Diagnosis" classification is unhelpful)
-
-    EID_drop_list = []
-
-    for row in range(dx.shape[0]):
-        if dx.iloc[row, 0] == 'No Diagnosis Given' or dx.iloc[row, 1] == 'No Diagnosis Given':
-            EID_drop_list.append(int(row))
-
-    dx = dx.drop(dx.index[EID_drop_list])
 
     # Create a dictionary containing codes and associated Dx
     code_dict = {}
@@ -101,6 +91,7 @@ def DSM():
         Dx_EID_list = []
         for col in range(dx.shape[1]):
             if col % 3 == 2 and not isinstance(dx.iloc[row, col], float):
+                # For Dx with ICD 10 Code that begins with 'F'
                 if 'F' in dx.iloc[row, col]:
                     Dx_EID_list.append('F' + dx.iloc[row, col].split('F', 1)[1][:2])
                     if ('F' + dx.iloc[row, col].split('F', 1)[1][:2]) not in code_dict.keys():
@@ -110,6 +101,7 @@ def DSM():
                             code_dict['F' + dx.iloc[row, col].split('F', 1)[1][:2]] = dx.iloc[
                                 row, col - 2]
 
+                # For Dx with ICD 10 Code that begins with 'Z'
                 elif 'Z' in dx.iloc[row, col]:
                     Dx_EID_list.append('Z' + dx.iloc[row, col].split('Z', 1)[1][:2])
                     if ('Z' + dx.iloc[row, col].split('Z', 1)[1][:2]) not in code_dict.keys():
@@ -119,21 +111,17 @@ def DSM():
                             code_dict['Z' + dx.iloc[row, col].split('Z', 1)[1][:2]] = dx.iloc[
                                 row, col - 2]
 
+                # For Dx that does not have an ICD 10 Code
+
+                elif 'No Diagnosis' in dx.iloc[row, col]:
+                    Dx_EID_list.append(dx.index[row])
+                    code_dict[dx.index[row]] = dx.iloc[row, col - 2]
                 else:
                     Dx_EID_list.append(dx.iloc[row, col-2])
 
         EID_Dx_dict[dx.index.values[row]] = Dx_EID_list
 
-    print(len(set(code_dict.values())))
-
-    # Create a dictionary where key = EID and values = diagnosis / diagnoses
-
     dict_duplicates_removed = {a: list(set(b)) for a, b in EID_Dx_dict.items()}
-
-    # print(code_dict)
-    # print(EID_Dx_dict)
-    # print(EID_Dx_dict['NDARAV945MCQ'])
-    # print(dict_duplicates_removed['NDARAK653RYE'])
 
     # Remove patients from DSM Data dataframe if they were removed from the Dx dataframe
 
@@ -154,7 +142,6 @@ def DSM():
 
     # Remove questions / patients with missing data, replace other NaN with mode
 
-    # mean_list = []
     mode_list = []
 
     for column in df:
@@ -162,7 +149,6 @@ def DSM():
             df = df.drop(column, axis=1)
 
     for column in df:
-        # mean_list.append(df[column].mean())
         freqs = groupby(Counter(column).most_common(), lambda x: x[1])
         modes = list(freqs)[0][0]
         mode_list.append(modes)
@@ -183,10 +169,22 @@ def DSM():
     df['train'] = np.random.uniform(0, 1, len(df)) <= .20
     df_copy = df
 
+    # Create MultiLabel Classifier and segment dataset for training/testing
+
+    training_set = df[df['train'] == True]
+    testing_set = df[df['train'] == False]
+
     for row in range(df.shape[0]):
         for col in range(df.shape[1]):
             if df.iloc[row, col] == 'NA':
                 # df_copy.iloc[row, col] = mean_list[col]
+                df_copy.iloc[row, col] = mode_list[col]
+
+    features = df.columns[3:-2]
+
+    for row in range(df.shape[0]):
+        for col in range(df.shape[1]):
+            if df.iloc[row, col] == 'NA':
                 df_copy.iloc[row, col] = mode_list[col]
 
     writer = pd.ExcelWriter('DSM_NaN_Replaced.xlsx')
