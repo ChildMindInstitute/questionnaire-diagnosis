fdc49380c12df18eed19a9416fad9ec827ea22a5
Jake Son <jake.son@childmind.org>
2017-09-25 16:33:51 -0400

    Successfully load appropriate train/test files corresponding to one of three sets.

--- RF.py
+++ RF.py
@@ -11,6 +11,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.svm import LinearSVC
 from sklearn.multiclass import OneVsRestClassifier
+from sklearn.metrics import f1_score
 
 def learn():
 
@@ -61,24 +62,31 @@ def learn():
     # print(list(zip(train_set[features], rnd_clf.feature_importances_)))
 
 
-def RF():
+def load(num):
 
-    filename = 'DSM_NaN_Replaced.xlsx'
-    sheetname = 'DSM_Data'
-    output = pd.ExcelFile(filename)
-    df = output.parse(sheetname)
-    df = pd.DataFrame(data=df)
-    df = df.set_index('EID')
+    train = 'DSM_Train' + str(num) + 'replaced.xlsx'
+    test = 'DSM_Test' + str(num) + 'replaced.xlsx'
 
-    train_set = df[df['train'] == True][0:100]
-    test_set = df[df['train'] == False][0:50]
+    output = pd.ExcelFile('/Users/jake.son/PycharmProjects/Dx_mvpa/Train_Test_Sets/' + train)
+    df_tr = output.parse('Train')
+    df_tr = pd.DataFrame(data=df_tr)
+    df_tr = df_tr.set_index('EID')
 
-    writer = pd.ExcelWriter('Train_Set.xlsx')
-    writer2 = pd.ExcelWriter('Test_Set.xlsx')
-    train_set.to_excel(writer, 'DSM_Data')
-    test_set.to_excel(writer2, 'DSM_Data')
-    writer.save()
-    writer2.save()
+    output = pd.ExcelFile('/Users/jake.son/PycharmProjects/Dx_mvpa/Train_Test_Sets/' + test)
+    df_te = output.parse('Test')
+    df_te = pd.DataFrame(data=df_te)
+    df_te = df_te.set_index('EID')
+
+    return df_tr, df_te
+
+
+def RF(df_tr, df_te):
+
+    train_set = df_tr
+    test_set = df_te
+
+    print(train_set.head)
+    print(test_set.head)
 
     train_targets = list(train_set['Dx'])
     test_targets = list(test_set['Dx'])
@@ -110,6 +118,8 @@ def RF():
 
     labels = mlb.inverse_transform(clf.predict(X_test))
 
+    print(X_test)
+
     from pprint import pprint
 
     pprint(labels)
@@ -119,5 +129,92 @@ def RF():
     # for item, labels in zip(X_test, labels):
     #     print('{1}'.format(item, ', '.join(labels)))
 
+
+def OneVsRF():
+
+    filename = 'DSM_NaN_Replaced.xlsx'
+    sheetname = 'DSM_Data'
+    output = pd.ExcelFile(filename)
+    df = output.parse(sheetname)
+    df = pd.DataFrame(data=df)
+    df = df.set_index('EID')
+
+    train_set = df[df['train'] == True][0:100]
+    test_set = df[df['train'] == False][0:50]
+    train_set['Anx'] = np.zeros(len(train_set)).astype('object')
+    train_set['ADHD'] = np.zeros(len(train_set)).astype('object')
+    test_set['Anx'] = np.zeros(len(test_set)).astype('object')
+    test_set['ADHD'] = np.zeros(len(test_set)).astype('object')
+    copy = train_set
+    copy2 = test_set
+
+    for row in range(train_set.shape[0]):
+        if 'Anxiety' in str(train_set.iloc[row, 607]):
+            copy.iloc[row, 609] = 'Anxiety'
+            copy.iloc[row, 610] = 'NA'
+        elif 'Attention' in str(train_set.iloc[row, 607]):
+            copy.iloc[row, 610] = 'ADHD'
+            copy.iloc[row, 609] = 'NA'
+        else:
+            copy.iloc[row, 609] = 'NA'
+            copy.iloc[row, 610] = 'NA'
+
+    for row in range(test_set.shape[0]):
+        if 'Anxiety' in str(train_set.iloc[row, 607]):
+            copy2.iloc[row, 609] = 'Anxiety'
+            copy2.iloc[row, 610] = 'NA'
+        elif 'Attention' in str(test_set.iloc[row, 607]):
+            copy2.iloc[row, 610] = 'ADHD'
+            copy2.iloc[row, 609] = 'NA'
+        else:
+            copy2.iloc[row, 609] = 'NA'
+            copy2.iloc[row, 610] = 'NA'
+
+    writer = pd.ExcelWriter('Train_Set.xlsx')
+    writer2 = pd.ExcelWriter('Test_Set.xlsx')
+    copy.to_excel(writer, 'DSM_Data')
+    copy2.to_excel(writer2, 'DSM_Data')
+    writer.save()
+    writer2.save()
+
+    clf2 = OneVsRestClassifier(LinearSVC())
+    clf3 = OneVsRestClassifier(LinearSVC())
+
+    Y = np.array(copy['Anx'])
+    Targ = np.array(copy2['Anx'])
+    Y2 = np.array(copy['ADHD'])
+    Targ2 = np.array(copy2['ADHD'])
+
+    X_train = train_set.columns[2:-4]
+    X_test = (test_set[X_train])
+    X_train = (train_set[X_train])
+
+    clf2.fit(X_train, Y)
+    clf3.fit(X_train, Y2)
+
+    count = 0
+    count2 = 0
+
+    preds = clf2.predict(X_test)
+    preds2 = clf3.predict(X_test)
+
+    for item in range(len(preds)):
+        if preds[item] == Targ[item]:
+            count += 1
+
+    print(preds)
+    print(f1_score(Targ, preds, average='weighted'))
+    print(count)
+
+    for item in range(len(preds2)):
+        if preds2[item] == Targ2[item]:
+            count2 += 1
+
+    print(preds2)
+    print(f1_score(Targ2, preds2, average='weighted'))
+    print(count2)
+
 if __name__ == '__main__':
-    RF()
+
+    [df_tr, df_te] = load(3)
+    # RF(df_tr, df_te)
