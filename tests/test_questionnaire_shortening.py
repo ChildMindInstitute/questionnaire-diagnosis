e523690e604e4d6d959cfbe4f0649e7273a6f6c6
Schaechtle <ulli@mit.edu>
2017-10-13 14:57:55 -0400

    Add better abstraction.

--- tests/test_questionnaire_shortening.py
+++ tests/test_questionnaire_shortening.py
@@ -56,14 +56,15 @@ def generate_data_disease(seed, N):
         'tests/data/noisy_or_n=%d_seed=%d.csv' % (N, seed),
         index=False
     )
+    return data
 
 def test_data_gen_smoke():
     generate_data_disease(1, 3)
     df = pd.read_csv('tests/data/noisy_or_n=3_seed=1.csv')
     assert len(df) == 3
     assert df.columns.shape[0] == 5
-    assert df.columns[0] == '0'
-    assert df.columns[1] == '1'
+    assert df.columns[0] == 'c_0'
+    assert df.columns[1] == 'c_1'
 
 
 
@@ -79,62 +80,9 @@ CREATE TABLE "temp" AS
       USING {n_samples} SAMPLES AS mi BY pop)
 '''
 
+def shorten_conditional_entropy(bdb, experimental_config):
 
-DESIRED_NUMBER_OF_QUESTIONS = 10
-
-NUMBER_MC_SAMPLES = [10, 100]
-MINUTES_ANALYSIS = [60, 240]
-DIAGNOSIS = {
-    'autism': "Autism Spectrum Disorder",
-    'adhd': "Attention-Deficit/Hyperactivity Disorder"
-}
-
-@pytest.mark.parametrize('number_mc_samples', [10, 50, 100])
-@pytest.mark.parametrize('number_datapoints', [10, 500])
-@pytest.mark.parametrize('number_iterations', [1000])
-@pytest.mark.parametrize('seed', range(1, 11))
-def test_noisy_or(number_mc_samples, number_datapoints, number_iterations, seed):
-    experimental_config = {
-        'number_mc_samples' : str(number_mc_samples),
-        'number_datapoints' : str(number_datapoints),
-        'number_iterations' : str(number_iterations),
-        'seed' : seed,
-    }
-
-    data  = generate_data_disease(seed, number_datapoints)
-    mkdir('tests/bdb/')
-    file_name = 'tests/bdb/test_noisy_or_mc={number_mc_samples}_n={number_datapoints}' +\
-        '_iters={number_iterations}_seed={seed}.bdb'
-    bdb_file_name = file_name.format(**experimental_config)
-    # XXX Great. I neither haven an idea why on earth one would make setting the
-    # seed so complicated, neither do I fully understand what struct.pack is
-    # actually doing.
-    byte_str_seed = struct.pack('<QQQQ', seed, seed, seed, seed)
-
-    if os.path.exists(bdb_file_name):
-        os.remove(bdb_file_name)
-    bdb = bayesdb_open(bdb_file_name, seed=byte_str_seed)
-    bdb.metamodels['cgpm'].set_multiprocess(False)
-
-    bdb.execute('''
-        CREATE TABLE data_table
-            FROM 'tests/data/noisy_or_n={number_datapoints}_seed={seed}.csv';
-    '''.format(**experimental_config))
-    column_names = query(bdb, 'SELECT * FROM data_table').columns.tolist()
-    bdb.execute('''
-        CREATE POPULATION pop FOR data_table WITH SCHEMA(
-            MODEL {column_names}
-            AS NOMINAL;
-        );
-    '''.format(column_names=','.join(column_names)))
-    bdb.execute('CREATE ANALYSIS SCHEMA cc FOR pop WITH BASELINE crosscat;')
-    bdb.execute('INITIALIZE 1 MODELS FOR cc;')
-    bdb.execute('''
-        ANALYZE cc FOR {number_iterations} ITERATION  WAIT(OPTIMIZED);
-    '''.format(**experimental_config))
-    start = time.time()
-
-    candidate_questions = column_names
+    candidate_questions = experimental_config['column_names']
     # Remove the diagnosis from the candidates.
     candidate_questions.remove('target')
     selected_questions = [] # Initialize: no questions selected yet.
@@ -160,7 +108,7 @@ def test_noisy_or(number_mc_samples, number_datapoints, number_iterations, seed)
 
             current_bql_str = BQL_STR.format(
                 columns=columns,
-                n_samples=number_mc_samples,
+                n_samples=experimental_config['number_mc_samples'],
                 diagnosis= 'target'
             )
             bdb.execute('DROP TABLE IF EXISTS "temp"')
@@ -179,6 +127,82 @@ def test_noisy_or(number_mc_samples, number_datapoints, number_iterations, seed)
 
         selected_questions.append(new_question)
         candidate_questions.remove(new_question)
+
+    return selected_questions
+
+def prep_bdb(experimental_config):
+    """ This functions prepare a bdb object.
+
+        It reads the csv file, creates the population and runs analysis. 
+    """
+    mkdir('tests/bdb/')
+    file_name = 'tests/bdb/test_noisy_or_mc={number_mc_samples}_n={number_datapoints}' +\
+        '_iters={number_iterations}_seed={seed}.bdb'
+    bdb_file_name = file_name.format(**experimental_config)
+    # XXX Great. I neither haven an idea why on earth one would make setting the
+    # seed so complicated, neither do I fully understand what struct.pack is
+    # actually doing.
+    byte_str_seed = struct.pack(
+        '<QQQQ',
+        experimental_config['seed'],
+        experimental_config['seed'],
+        experimental_config['seed'],
+        experimental_config['seed'],
+    )
+
+    if os.path.exists(bdb_file_name):
+        os.remove(bdb_file_name)
+
+    bdb = bayesdb_open(bdb_file_name, seed=byte_str_seed)
+    bdb.metamodels['cgpm'].set_multiprocess(False)
+    bdb.execute('''
+        CREATE TABLE data_table
+            FROM 'tests/data/noisy_or_n={number_datapoints}_seed={seed}.csv';
+    '''.format(**experimental_config))
+    bdb.execute('''
+        CREATE POPULATION pop FOR data_table WITH SCHEMA(
+            MODEL {column_names}
+            AS NOMINAL;
+        );
+    '''.format(column_names=','.join(experimental_config['column_names'])))
+
+    bdb.execute('CREATE ANALYSIS SCHEMA cc FOR pop WITH BASELINE crosscat;')
+    bdb.execute('INITIALIZE 1 MODELS FOR cc;')
+    bdb.execute('''
+        ANALYZE cc FOR {number_iterations} ITERATION  WAIT(OPTIMIZED);
+    '''.format(**experimental_config))
+    return bdb
+
+DESIRED_NUMBER_OF_QUESTIONS = 10
+
+NUMBER_MC_SAMPLES = [10, 100]
+MINUTES_ANALYSIS = [60, 240]
+DIAGNOSIS = {
+    'autism': "Autism Spectrum Disorder",
+    'adhd': "Attention-Deficit/Hyperactivity Disorder"
+}
+
+@pytest.mark.parametrize('number_mc_samples', [1])
+@pytest.mark.parametrize('number_datapoints', [1])
+@pytest.mark.parametrize('number_iterations', [1])
+@pytest.mark.parametrize('seed', range(1, 2))
+def test_noisy_or(number_mc_samples, number_datapoints, number_iterations, seed):
+
+    experimental_config = {
+        'number_mc_samples' : str(number_mc_samples),
+        'number_datapoints' : str(number_datapoints),
+        'number_iterations' : str(number_iterations),
+        'seed'              : seed,
+        'column_names'      : generate_data_disease(seed, number_datapoints).keys()
+    }
+
+    # Prepare bdb given an experimental configuration.
+    bdb = prep_bdb(experimental_config)
+
+    # Select columns.
+    selected_questions = shorten_conditional_entropy(bdb, experimental_config)
+
+    # Save result.
     mkdir('tests/output/selected/')
     output_file_name = 'tests/output/selected/questions_noisy_or_mc={number_mc_samples}' +\
         'n={number_datapoints}_iters={number_iterations}_seed={seed}.csv'
