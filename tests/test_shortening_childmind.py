85b50d59c69a9d218785051ab94a3bdd077d100a
Schaechtle <ulli@mit.edu>
2017-11-03 11:26:08 -0400

    Refactor questionnaire shortening on childmind to draw on a standard code base.

--- tests/test_shortening_childmind.py
+++ tests/test_shortening_childmind.py
@@ -29,19 +29,11 @@ from bayeslite import bayesdb_open
 from bayeslite import bayesdb_read_csv
 from iventure.utils_bql import query
 
+import sys
+sys.path.append('src')
+from quantify_predictive_value import select_columns
+from quantify_predictive_value import get_all_scoring_functions
 from testing_utils import mkdir
-
-
-BQL_STR = '''
-CREATE TABLE "temp" AS
-  SELECT entropy, mi FROM
-    (ESTIMATE MUTUAL INFORMATION OF
-      ({columns}) WITH ({columns})
-      USING {n_samples} SAMPLES AS entropy BY pop),
-    (ESTIMATE MUTUAL INFORMATION OF
-      ({columns}) WITH "{diagnosis}"
-      USING {n_samples} SAMPLES AS mi BY pop)
-'''
 
 DESIRED_NUMBER_OF_QUESTIONS = 1
 
@@ -52,33 +44,32 @@ DIAGNOSIS = {
     'adhd': "Attention-Deficit/Hyperactivity Disorder"
 }
 
+def prep_bdb(experimental_config):
+    """ This functions prepare a bdb object.
 
-@pytest.mark.parametrize('number_mc_samples', NUMBER_MC_SAMPLES)
-@pytest.mark.parametrize('minutes_analysis', MINUTES_ANALYSIS)
-@pytest.mark.parametrize('diagnosis', DIAGNOSIS.keys())
-@pytest.mark.parametrize('seed', range(1, 2))
-def test_questinnaire_short(number_mc_samples, minutes_analysis, diagnosis, seed):
-    experimetal_config = {
-        'number_mc_samples' : str(number_mc_samples),
-        'minutes_analysis' : str(minutes_analysis),
-        'diagnosis': diagnosis,
-        'seed' : seed,
-    }
-
+        It reads the csv file, creates the population and runs analysis.
+    """
     mkdir('tests/bdb/')
-    file_name = 'tests/bdb/test_questinnaire_{diagnosis}_short_mc={number_mc_samples}' +\
+    file_name = 'tests/bdb/test_childmind_{data_generator_name}_' +\
+        '{scoring_function_name}_mc={number_mc_samples}' +\
         '_minutes={minutes_analysis}_seed={seed}.bdb'
-    bdb_file_name = file_name.format(**experimetal_config)
+    bdb_file_name = file_name.format(**experimental_config)
     # XXX Great. I neither haven an idea why on earth one would make setting the
     # seed so complicated, neither do I fully understand what struct.pack is
     # actually doing.
-    byte_str_seed = struct.pack('<QQQQ', seed, seed, seed, seed)
+    byte_str_seed = struct.pack(
+        '<QQQQ',
+        experimental_config['seed'],
+        experimental_config['seed'],
+        experimental_config['seed'],
+        experimental_config['seed'],
+    )
 
     if os.path.exists(bdb_file_name):
         os.remove(bdb_file_name)
+
     bdb = bayesdb_open(bdb_file_name, seed=byte_str_seed)
     bdb.metamodels['cgpm'].set_multiprocess(False)
-
     bdb.execute('''
         CREATE TABLE data_table
             FROM 'resources/data/init_data.csv';
@@ -116,67 +107,61 @@ def test_questinnaire_short(number_mc_samples, minutes_analysis, diagnosis, seed
                  "EID";
         );
     ''')
+
     bdb.execute('CREATE ANALYSIS SCHEMA cc FOR pop WITH BASELINE crosscat;')
     bdb.execute('INITIALIZE 1 MODELS FOR cc;')
+    #bdb.execute('''
+    #    ANALYZE cc FOR {minutes_analysis} MINUTES WAIT(OPTIMIZED);
+    #'''.format(**experimental_config))
+
     bdb.execute('''
-        ANALYZE cc FOR {minutes_analysis} MINUTES WAIT(OPTIMIZED);
-    '''.format(**experimetal_config))
+        ANALYZE cc FOR 1 ITERATIONS WAIT(OPTIMIZED);
+    '''.format(**experimental_config))
+    return bdb
 
-    # Get the names of all candidate variables for the shortened questionnaire.
-    df = query(bdb, 'SELECT * FROM data_table LIMIT 1')
-    candidate_questions = df.columns.tolist()
-    # Remove the diagnosis from the candidates.
-    candidate_questions.remove(DIAGNOSIS[diagnosis])
-    # Remove the subject ID from the candidates.
-    candidate_questions.remove("EID")
+@pytest.mark.parametrize('scoring_function_name', get_all_scoring_functions())
+@pytest.mark.parametrize('number_mc_samples', NUMBER_MC_SAMPLES)
+@pytest.mark.parametrize('minutes_analysis', MINUTES_ANALYSIS)
+@pytest.mark.parametrize('diagnosis', DIAGNOSIS.keys())
+@pytest.mark.parametrize('seed', range(1, 2))
+def test_questionnaire_short(
+        scoring_function_name,
+        number_mc_samples,
+        minutes_analysis,
+        diagnosis,
+        seed
+    ):
+    experimental_config = {
+        'target' : DIAGNOSIS[diagnosis],
+        'scoring_function_name' : scoring_function_name,
+        'scoring_function' : get_all_scoring_functions()[scoring_function_name],
+        'data_generator_name' : 'childmind',
+        'number_mc_samples' : str(number_mc_samples),
+        'minutes_analysis' : str(minutes_analysis),
+        'seed'              : seed,
+        'desired_number_of_questions': DESIRED_NUMBER_OF_QUESTIONS,
+    }
+    # Prepare bdb given an experimental configuration.
+    bdb = prep_bdb(experimental_config)
+    experimental_config['column_names'] = query(bdb, '''
+        SELECT * FROM data_table LIMIT 1;
+    ''').columns.tolist()
+    experimental_config['column_names'].remove('EID')
 
-    selected_questions = [] # Initialize: no questions selected yet.
+    # Select columns.
+    selected_columns = select_columns(bdb, experimental_config)
 
-    # While we don't have the desired number of quesions, keep searching.
-    bdb.sql_execute('DROP TABLE IF EXISTS "cond_entropy"')
-    bdb.sql_execute('CREATE TABLE cond_entropy(entropy REAL, mi REAL, columns TEXT)')
-
-    while len(selected_questions) < DESIRED_NUMBER_OF_QUESTIONS:
-        current_scores = []
-        # Loop through all the candidate questions.
-
-        if selected_questions:
-            selected_columns = ','.join(selected_questions)
-        else:
-            selected_columns = 'None'
-
-        for next_question in candidate_questions:
-            columns = ','.join([
-                    '"%s"' % question for question in
-                    [next_question] + selected_questions
-                ])
-
-            current_bql_str = BQL_STR.format(
-                columns=columns,
-                n_samples=number_mc_samples,
-                diagnosis= DIAGNOSIS[diagnosis]
-            )
-            bdb.execute('DROP TABLE IF EXISTS "temp"')
-            bdb.execute(current_bql_str)
-            bdb.sql_execute('INSERT INTO cond_entropy (entropy, mi, columns)' +
-                '''SELECT entropy, mi, '{selected_columns}' FROM
-                "temp"'''.format(selected_columns=selected_columns)
-            )
-
-        df = query(bdb, '''
-            SELECT entropy - mi AS conditional_entropy FROM cond_entropy
-                WHERE columns = '{selected_columns}'
-        '''.format(selected_columns=selected_columns))
-
-        new_question = candidate_questions[np.argmin(df['conditional_entropy'].values)]
-
-        selected_questions.append(new_question)
-        candidate_questions.remove(new_question)
-    mkdir('tests/output/selected/childmind')
-    output_file_name = 'tests/output/selected/childmind/questions_{diagnosis}_mc={number_mc_samples}' +\
-        '_minutes={minutes_analysis}_seed={seed}.csv'
-    pd.DataFrame({'selected':selected_questions}).to_csv(
-        output_file_name.format(**experimetal_config),
-        index=False
+    # Save result.
+    mkdir(
+        'tests/output/childmind/{scoring_function_name}'.format(
+            **experimental_config
+         )
     )
 
+    output_file_name = 'tests/output/childmind/{scoring_function_name}/' +\
+        'selected_columns_childmind_mc={number_mc_samples}' +\
+        '_minutes={minutes_analysis}_seed={seed}.csv'
+    pd.DataFrame({'selected':selected_columns}).to_csv(
+        output_file_name.format(**experimental_config),
+        index=False
+    )
